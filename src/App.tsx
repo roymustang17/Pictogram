aab8b19fc5d5f4388dbb6b16edd25ad7e2c7b085
Arjun A <arjunaggarwal1710@gmail.com>
2024-01-11 20:52:53 +0530

    Basic setup and routes.

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import { Routes, Route } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import { Home } from "./_root/pages";
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes  */}
+        <Route path="/sign-in" element={<SigninForm />} />
+        <Route path="/sign-up" element={<SignupForm />} />
+
+        {/* private routes  */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
