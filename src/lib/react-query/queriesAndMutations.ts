9757dd524701bff5da982a0952f18e0ac29db111
Arjun A <arjunaggarwal1710@gmail.com>
2024-02-05 16:46:09 +0530

    Create Post

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,11 +1,13 @@
-import { INewUser } from '@/types'
+import { INewPost, INewUser } from '@/types'
 import {
     useQuery,
     useMutation,
     useQueryClient,
     useInfiniteQuery,
+    QueryClient,
 } from '@tanstack/react-query'
-import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
+import { createPost, createUserAccount, getRecentPosts, signInAccount, signOutAccount } from '../appwrite/api'
+import { QUERY_KEYS } from './queryKeys'
 
 
 export const useCreateUserAccount = () => {
@@ -23,5 +25,24 @@ export const useSignInAccount = () => {
 export const useSignOutAccount = () => {
     return useMutation({
         mutationFn: signOutAccount
+    })
+}
+
+export const useCreatepost = () => {
+
+    return useMutation({
+        mutationFn: (post: INewPost) => createPost(post),
+        onSuccess: ()=>{
+            QueryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
+            })
+        }
+    })
+}
+
+export const useGetRecentPosts = () => {
+    return useQuery({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        queryFn: getRecentPosts,
     })
 }
