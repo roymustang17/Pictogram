df30f2a1352090df857053bd058873afbf07a524
Arjun A <arjunaggarwal1710@gmail.com>
2024-02-06 19:08:54 +0530

    Post Card

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -42,7 +42,7 @@ const Home = () => {
             <ul className="flex flex-col flex-1 gap-9 w-full ">
               {posts?.documents.map((post: Models.Document) => (
                 <li key={post.$id} className="flex justify-center w-full">
-                  <PostCard post={post} />
+                  <PostCard post={post} key={post.caption} />
                 </li>
               ))}
             </ul>
