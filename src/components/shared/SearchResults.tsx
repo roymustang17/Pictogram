e542a506d4c4d14a78ea8eb81dbd6f0b56b03918
Arjun A <arjunaggarwal1710@gmail.com>
2024-02-10 02:37:46 +0530

    Explore page and search

---
+++ src/components/shared/SearchResults.tsx
@@ -0,0 +1,24 @@
+import { Models } from "appwrite";
+import Loader from "./Loader";
+import GridPostList from "./GridPostList";
+import { searchPosts } from "@/lib/appwrite/api";
+
+type SearchresultsProps = {
+  isSearchFetching: boolean;
+  searchedPosts: Models.Document[];
+};
+
+const SearchResults = ({
+  isSearchFetching,
+  searchedPosts,
+}: SearchresultsProps) => {
+  if (isSearchFetching) return <Loader />;
+  if (searchedPosts && searchedPosts.documents.length > 0) {
+    return <GridPostList posts={searchPosts.documents} />;
+  }
+  return (
+    <p className="text-light-4 mt-10 text-center w-full">No results found</p>
+  );
+};
+
+export default SearchResults;
